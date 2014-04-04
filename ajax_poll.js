89b60e2bf121965485e2bda6276ca251c71a2c79
Jay Callicott <jaykali@42248.no-reply.drupal.org>
2014-04-04 13:40:56 -0500

    Refactored a lot of output to make it work back and forth with messages

--- ajax_poll.js
+++ ajax_poll.js
@@ -13,39 +13,21 @@ Drupal.behaviors.ajaxPoll.attach = function(context) {
   $('form.ajax-poll:not(.ajax-poll-processed)', context).addClass('ajax-poll-processed').each(function() {
     // Find the form and poll wrapper items that will be affected.
     var $form = $(this);
-    var $initialTarget;
     var $pollWrapper;
-
-    if ($form.hasClass('ajax-vote')) {
-      $initialTarget = $form;
-    }
-    else {
-      $initialTarget = $form.closest('.poll');
-    }
-
-    $pollWrapper = $initialTarget.closest(".ajax-poll-wrapper");
-
-    if ($pollWrapper.length == 0) {
-      $initialTarget.wrap('<div class="ajax-poll-wrapper" />');
-
-      $pollWrapper = $initialTarget.closest(".ajax-poll-wrapper");
-    }
 
     // Find all the settings for this form.
     var url = $form.find('input[name=ajax_url]').val();
     var disabledText = $form.find('input[name=ajax_text]').val();
     var enabledText = $form.find('input.form-submit').val();
 
-    if (url.indexOf('vote') > 0)
-      $pollWrapper = $form.parents('.ajax-poll-wrapper:first');
-    else
-      $pollWrapper = $form.parents('.poll:first');
+    // Find container, either the ajax poll wrapper, or the results wrapper div.
+    $pollWrapper = $form.closest(".ajax-poll-wrapper, .poll-results");
 
     // Disable submit button until a choice has been made.
-    $pollWrapper.find('input.form-submit').attr('disabled', 'disabled');
+    $pollWrapper.find('.poll-view-voting input.form-submit').attr('disabled', 'disabled');
 
     $('input[name=choice]').click(function() {
-      $pollWrapper.find('input.form-submit').removeAttr('disabled');
+      $pollWrapper.find('.poll-view-voting input.form-submit').removeAttr('disabled');
     });
 
     // Set up the options for the AJAX voting mechanism.
@@ -53,34 +35,30 @@ Drupal.behaviors.ajaxPoll.attach = function(context) {
       url: url,
       beforeSubmit: function(values, form, options) {
         if (disabledText) {
-          $form.find('input.form-submit').attr('disabled', true).val(disabledText);
+          $form.find('input.form-submit').attr('disabled', 'disabled').val(disabledText);
         }
       },
       success: function(response, status) {
-        // Remove previous messages and re-enable the buttons in case anything
-        // goes wrong after this.
-        $form.find('input.form-submit').attr('disabled', '').val(enabledText);
-        $form.find('.messages').remove();
-
         // On success, replace the poll content with the new content.
         if (response.status) {
-          $pollWrapper.replaceWith(response.output);
+          $('.messages').remove();
+
+          $pollWrapper.replaceWith(response.messages + response.output);
+
           // The action attribute will be the path of the AJAX Poll menu
           // callback. We fix this here for consistency, even though this will
           // be replaced by the AJAX Poll beforeSubmit() function above.
           $pollWrapper.find('form').attr('action', window.location.pathname);
-          Drupal.attachBehaviors($pollWrapper.parent().get(0));
-        }
 
-        // Display any new messages.
-        if (response.messages) {
-          $pollWrapper.prepend(response.messages);
+          Drupal.attachBehaviors($pollWrapper.parent().get(0));
         }
       },
       complete: function(response, status) {
         $form.find('input.form-submit').attr('disabled', '').val(enabledText);
 
         if (status == 'error' || status == 'parsererror') {
+          $('.messages').remove();
+          
           $form.prepend(Drupal.theme('ajaxPollError'));
         }
       },
